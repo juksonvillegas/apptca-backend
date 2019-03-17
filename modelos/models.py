39661d5a8267083090f3764222951d4812a786ce
jukson villegas <juksonvillegas@gmail.com>
2019-03-17 01:44:51 -0500

    marcas y modelos se agrego campo estado, a modelos se agrego campo marca_nombre para obtener el nombre de la marca

--- modelos/models.py
+++ modelos/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Marca(models.Model):
     nombre = models.CharField(max_length = 50)
+    estado = models.BooleanField(default=True)
     def __str__(self):
         return self.nombre
 
@@ -10,4 +11,5 @@ class Modelo(models.Model):
     nombre = models.CharField(max_length=50)
     extendido = models.CharField(max_length=200)
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    estado = models.BooleanField(default=True)
 
--- modelos/serializers.py
+++ modelos/serializers.py
@@ -4,9 +4,10 @@ from rest_framework import serializers
 class MarcaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Marca
-        fields = ('id','nombre')
+        fields = ('id','nombre','estado')
 
 class ModeloSerializer(serializers.ModelSerializer):
+    marca_nombre = serializers.CharField(source='marca.nombre', required=False, read_only=True)
     class Meta:
         model = Modelo
-        fields = ('id','nombre','extendido','marca')
+        fields = ('id','nombre', 'extendido', 'marca' ,'estado', 'marca_nombre')
